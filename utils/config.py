0477eeef8f0b94007c7fbe7cdb486bc570de6d54
tsotfsk <tsotfsk@qq.com>
2021-06-02 23:20:29 +0800

    FEA: add config & gitignore

--- utils/config.py
+++ utils/config.py
@@ -4,17 +4,18 @@ parser = argparse.ArgumentParser()
 
 # files
 parser.add_argument('--output_dir', type=str, default='./saved/', help='')
-parser.add_argument('--log_fire', type=str,
+parser.add_argument('--log_file', type=str,
                     default='./saved/', help='basket_group_split')
 
 # main hyps
-parser.add_argument('--evalaute', action='store_true')
+parser.add_argument('--evaluate', action='store_true')
+parser.add_argument('--commit', type=str, default='')
 parser.add_argument('--model', type=str, default='GRU4Rec')
 parser.add_argument('--epochs', type=int, default=50,
                     help='the number of epochs to train for')
 parser.add_argument('--lr', type=float, default=0.001,
                     help='learning rate')  # [0.001, 0.0005, 0.0001]
-parser.add_argument('--batch_size', type=int, default=1024, help='batch_size')
+parser.add_argument('--batch_size', type=int, default=2048, help='batch_size')
 parser.add_argument('--dropout_prob', type=float, default=0.3, help='dropout')
 parser.add_argument('--embedding_size', type=int,
                     default=64, help='input sise')
@@ -22,7 +23,7 @@ parser.add_argument('--hidden_size', type=int, default=64, help='hidden sise')
 parser.add_argument('--num_layers', type=int, default=1, help='gru num_layer')
 
 # others
-parser.add_argument('--device', type=int, default=0, help='GPU_ID')
+parser.add_argument('--device', type=int, default=1, help='GPU_ID')
 parser.add_argument('--seed', type=int, default=24, help='seed')
 parser.add_argument('--early_stop', type=int, default=10, help='gru num_layer')
 
