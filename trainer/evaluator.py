2ac4b5f99cc096314663b8ba38e282ac4f04d8ce
tsotfsk <tsotfsk@qq.com>
2021-06-04 19:16:37 +0800

    FEA: fix mrr

--- trainer/evaluator.py
+++ trainer/evaluator.py
@@ -4,20 +4,23 @@ import numpy as np
 
 class FullEvaluator(object):
 
-    def __init__(self, metrics=None, topk=10, pos_len=1):
+    def __init__(self, metrics=None, topk=20, pos_len=1):
         self.topk = topk
         self.metrics = metrics
         self.pos_len = pos_len
 
     def collect(self, scores, batch_data):
         # intermediate variables
-        target_ids = batch_data['target_ids'].cpu().numpy()
+        target_ids = batch_data['target_ids'].cpu()
         # idx = (torch.argmax(scores, dim=-1)).cpu().numpy()  # nusers x k
 
-        idx = torch.topk(scores, k=self.topk, dim=1)[1].cpu().numpy()
+        idx = torch.topk(scores, k=self.topk, dim=1)[1].cpu()
 
-        return ((target_ids[:, np.newaxis] - idx) == 0).sum(1), idx
+        return ((idx - target_ids.unsqueeze(1)) == 0), idx
 
     def evaluate(self, matrix):
-        pos_idx = np.concatenate(matrix, axis=0)
-        return pos_idx.sum() / pos_idx.shape[0]
+        pos_idx = torch.cat(matrix, axis=0).float()
+        ranks = 1 / (pos_idx.argmax(dim=1) + 1)
+        mask = pos_idx.sum(1)
+        result = ranks * mask
+        return result.sum() / pos_idx.shape[0]
